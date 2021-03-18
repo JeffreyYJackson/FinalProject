91fe7d39a39503a01a4c6a0f21b0c73af4fad47b
jeffreyj <jeffrey.y.jackson@gmail.com>
2021-03-18 23:06:57 +0300

    18.03.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Product Name Invalid";
+        public static string ProductListed = "Products Listed";
+        public static string MaintenanceTime = "System At Maintenance";
+    }
+}
